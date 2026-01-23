7ed1175f00f9d83cf0862d7c23305aa150f1e850
Peter Nilsson <peter8nilsson@live.se>
2026-01-23 08:36:43 +0100

    [utils] Add stacktrace utility

---
+++ src/utils/stacktrace.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#if defined(__GLIBC__)
+
+#include <execinfo.h>
+
+static constexpr size_t SPK_MAX_STACKTRACE_ENTRIES = 12;
+
+inline static char *
+__spk_stacktrace_entry_addr (char *entry)
+{
+    auto addr = strtok (entry, "(");
+    addr = strtok (nullptr, ")");
+    return addr;
+}
+
+inline static void
+spk_dump_stacktrace ()
+{
+    printf ("Stacktrace:\n");
+    static void *addrs[SPK_MAX_STACKTRACE_ENTRIES] = { 0 };
+    auto num_entries = backtrace (addrs, SPK_MAX_STACKTRACE_ENTRIES);
+    auto symbols = backtrace_symbols (addrs, num_entries);
+    for (int32_t i = 0; i < num_entries; ++i) {
+        auto addr = __spk_stacktrace_entry_addr (symbols[i]);
+        static char buf[256] = { 0 };
+        memset (buf, 0, sizeof (buf));
+        snprintf (buf, sizeof (buf), "addr2line -f -C -i -e bld/spk-interp %s", addr);
+        auto fd = popen (buf, "r");
+        memset (buf, 0, sizeof (buf));
+
+        // Store function name in first half of buffer
+        fgets (buf, 128, fd);
+        *strchr (buf, '\n') = 0;
+
+        // Store file path in second half of buffer
+        fgets (buf + 128, 128, fd);
+        *strchr (buf + 128, '\n') = 0;
+
+        printf ("%d> %s: %s\n", i, buf, buf + 128);
+        pclose (fd);
+    }
+    free (symbols);
+}
+
+#else
+
+inline static void
+spk_dump_stacktrace ()
+{
+}
+
+#endif
+
